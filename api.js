e415bf8168d3d4570f8ec17c2b28e4f6087f8b16
surajkale600518 <surajkale0502@gmail.com>
2024-09-15 12:07:15 +0530

    S

--- api.js
+++ api.js
@@ -1,26 +1,25 @@
-var express = require('express');
-var cors = require('cors');
+var express = require("express");
+var cors = require("cors");
 require('dotenv').config();
-var mongodb = require('mongodb').MongoClient;
-// let datastring = "mongodb://localhost:27017/"
-const datastring = process.env.DB_URL;
-const PORT = process.env.PORT || 3001;
-const app = express();
+var mongoClient = require("mongodb").MongoClient;
+var conString = process.env.DB_URL;
+const PORT = process.env.PORT || 7000;
+
+
+var app = express();
 app.use(cors());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended:true})); 
 app.use(express.json());
 
-app.get('/home',(req,res)=>{
-    mongodb.connect(datastring).then(objectClient=>{
-        let dabase = objectClient.db('e-com');
-        dabase.collection('product').find().toArray({}).then((document)=>{
-            res.send(document);
+app.get("/home", (req, res)=>{
+    mongoClient.connect(conString).then(clientObject=>{
+        var database  = clientObject.db("e-com");
+        database.collection("product").find({}).toArray().then(documents=>{
+            res.send(documents);
             res.end();
-        })
-    })
+        });
+    });
 });
 
-app.listen(PORT)
-console.log(`Server is Start... http://127.0.0.1:${PORT}`)
-   
-
+app.listen(PORT);
+console.log(`server is start... http://127.0.0.1:7000`)
